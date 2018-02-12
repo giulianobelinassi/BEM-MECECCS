c97409e5e0534fd422ae92325546b348bdc2752d
Giuliano Belinassi <giuliano.belinassi@usp.br>
2018-02-12 18:41:26 -0200

    bench.sh for tests

--- src/bench.sh
+++ src/bench.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-MESH_SIZE=(510 1820)
+MESH_SIZE=(510 1820 6840 26480)
 COMPILER_PARAMS=(\  gpu)
 MODE_STR=(cpu gpu)
-
-NUM_WARMUPS=1
-NUM_EXECUTIONS=3
 
 NUM_MODES=${#COMPILER_PARAMS[@]}
 NUM_MESHES=${#MESH_SIZE[@]}
@@ -30,14 +27,22 @@ for ((i=0; i<${NUM_MODES}; i++)); do
 			file_dyn=E${mesh}d.dat
 			sol_sta=S${mesh}e_${MODE_STR[$i]}_${thread}.dat 
 			sol_dyn=S${mesh}d_${MODE_STR[$i]}_${thread}.dat
-		
+			
+			warmups=3
+			executions=30
+
+			if (( $thread == 1 )); then
+				warmups=1
+				executions=5
+			fi
+
 			echo "Executando para $mesh em modo $mode_str com $thread threads"
 			echo ""
-			for ((l=1;l<=NUM_WARMUPS;l++)); do
+			for ((l=1;l<=warmups;l++)); do
 				echo "Warmup ${l}: "
 				./main $file_sta $file_dyn $sol_sta $sol_dyn
 			done
-			for ((l=1;l<=NUM_EXECUTIONS;l++)); do
+			for ((l=1;l<=executions;l++)); do
 				echo "Execução número $l"
 				output_file="results/results_${mode_str}_${mesh}_${thread}_${l}.txt"
 				./main $file_sta $file_dyn $sol_sta $sol_dyn ${extra_flag} >> ${output_file}
